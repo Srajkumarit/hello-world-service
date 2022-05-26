49c10b36d99fb564372a4b3597b71a964b77936d
srajkumarit <srajkumarit@hotmail.com>
2022-05-25 23:40:49 -0500

    Commit Project

---
+++ src/main/java/com/demo/helloworldservice/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.demo.helloworldservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World, Spring Boot!";
+    }
+
+}
